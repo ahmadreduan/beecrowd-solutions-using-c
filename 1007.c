6b86ea4d6846a7f246e573aa294a22266b5c2a0e
Reduan Ahmad <143122014+ahmadreduan@users.noreply.github.com>
2024-03-05 00:51:36 +0600

    Create 1007.c
    beecrowd | 1007
    Difference
    Adapted by Neilor Tonin, URI  Brazil
    
    Timelimit: 1
    Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
    
    Input
    The input file contains 4 integer values.
    
    Output
    Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, according to the following example, with a blank space before and after the equal signal.

---
+++ 1007.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+    int a, b, c, d, x;
+
+    scanf("%i %i %i %i", &a, &b, &c, &d);
+
+    x=(a*b)-(c*d);
+
+    printf("DIFERENCA = %i\n", x);
+
+    return 0;
+}
