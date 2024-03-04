6fb1e92579e00639c84b9b2a2867cc807d7ea1c4
Reduan Ahmad <143122014+ahmadreduan@users.noreply.github.com>
2024-03-05 00:48:15 +0600

    Create 1005.c
    Average 1

---
+++ 1005.c
@@ -0,0 +1,16 @@
+//Reduan Ahmad
+#include <stdio.h>
+
+int main(void)
+{
+    double a, b, x;
+    scanf("%lf %lf", &a, &b);
+    a=a*3.5;
+    b=b*7.5;
+  
+    x=a+b;
+    x=x/11;
+  
+    printf("MEDIA = %.5lf\n", x);
+    return 0;
+}
