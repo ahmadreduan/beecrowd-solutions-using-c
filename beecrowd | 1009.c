be18bca00c94db8c858e40e16a81d0ffe20a39ab
Reduan Ahmad <143122014+ahmadreduan@users.noreply.github.com>
2024-03-16 05:25:37 +0600

    Update beecrowd | 1009.c

--- beecrowd | 1009.c
+++ beecrowd | 1009.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-//Reduan Ahmad
-int main(void)
-
-{
+int main(void){
     char str[81];
-    double a, b, x;
+    double a, b, to;
 
     scanf("%s", str);
     scanf("%lf %lf", &a, &b);
-
-    x=a+(b*0.15);
-
-    printf("TOTAL = R$ %.2lf\n", x);
+    to=a+(b*0.15);
+    printf("TOTAL = R$ %.2lf\n", to);
 
     return 0;
 }
